63235623d13a8b82dff1ca6b4ba1deb0775d45a4
Kowus <barnabas@elite-education.org>
2017-11-04 08:40:58 +0000

    inits

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/webhook', (req, res)=>{
+    if(req.query['hub.mode']&& req.query['hub.verify_token'] === process.env.FACEBOOK_WEBHOOK_VERIFY_TOKEN){
+        res.status(200).send(req.query['hub.challenge']);
+    }else {
+        res.status(403).end();
+    }
+});
+
+app.post('/webhook', (req, res)=>{
+    console.log(req.body);
+    if(req.body.object === 'page')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const server = app.listen(process.env.PORT || 3000, ()=>{
+   console.log('Now my watch has began Lord %d of House %s', server.address().port, app.settings.env);
+});
